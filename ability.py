b48bf5c5b8c8fb17a764e2dbf9032315fa636d3e
xScoopy <JLeary070@gmail.com>
2020-11-12 20:39:41 -0600

    finished section 3

---
+++ ability.py
@@ -0,0 +1,20 @@
+import random
+
+
+class Ability:
+    def __init__(self, name, max_damage):
+        '''
+        Initialize the values passed into this method as instance variables.
+        '''
+        self.name = name
+        self.max_damage = max_damage
+
+    def attack(self):
+        ''' Return a value between 0 and the value set by self.max_damage. '''
+        random_value = random.randint(0, self.max_damage)
+        return random_value
+
+if __name__ == "__main__":
+    ability = Ability("Debugging Ability",20)
+    print(ability.name)
+    print(ability.attack())
---
+++ armor.py
@@ -0,0 +1,22 @@
+import random
+class Armor():
+    def __init__(self, name, max_block):
+        '''Instantiate instance properties. 
+        name: string
+        max_block: int
+        '''
+        self.name = name
+        self.max_block = max_block
+    
+    def block(self):
+        ''' 
+        Return a random value between 0 and the initialized
+        max_block strength.
+        '''
+        random_value = random.randint(0, self.max_block)
+        return random_value
+    
+if __name__ ==  "__main__":
+    armor = Armor("Debugging Shield", 10)
+    print(armor.name)
+    print(armor.block())
